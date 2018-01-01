756e7f403a9729856d9f6770a9031ec6800b3928
Nick Leeper <nick.leeper@gmail.com>
2017-12-31 18:36:29 -0600

    remove Spotify searching; play Spotify URI passed in

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,6 @@ const RC = require('rc')('ROSIE', require('../../config/default.json'))
 
 module.exports = {
   PORT: RC.PORT,
-  SPOTIFY: RC.SPOTIFY,
   PIPELINE: RC.PIPELINE,
   SONOS: RC.SONOS
 }
